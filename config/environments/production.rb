a14df11b57818697df4b2cc7b6a43e762ebaa196
Jeremy Daer <jeremydaer@gmail.com>
2025-12-10 17:36:23 -0800

    Enforce CSP (#25)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = { domain: "app.fizzy.do", address: "smtp-outbound", port: 25, enable_starttls_auto: false }
 
   # Content Security Policy
-  config.x.content_security_policy.report_only = true
+  config.x.content_security_policy.report_only = false
   config.x.content_security_policy.report_uri = "https://o33603.ingest.us.sentry.io/api/4510481339187200/security/?sentry_key=9f126ba30d5f703451a13a2929bb5a10"
   config.x.content_security_policy.script_src = "https://challenges.cloudflare.com"
   config.x.content_security_policy.frame_src = "https://challenges.cloudflare.com"
